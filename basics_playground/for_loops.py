60a5826edad4aa545eb163f54f069b145a4ba9bb
Josiah McPeak <mcpeakjosiah@gmail.com>
2018-04-02 10:57:21 -0700

    Not sure what is going to show up here.

---
+++ basics_playground/for_loops.py
@@ -0,0 +1,9 @@
+
+arr = ["yellow", 88, 99, "pink", "whatup", 1, 1]
+arr2 = [4, 5, 6, 7, 99]
+def display_vals(arr):
+    for item in arr:
+        print item
+
+display_vals(arr)
+display_vals(arr2)
